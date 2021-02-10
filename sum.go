0bb32c54f2d00a884516b49b2d4e612017a1f11b
matheus0214 <matheusgiga123@gmail.com>
2021-02-10 15:13:44 -0300

    ci: Check is qualit gate is working

--- sum.go
+++ sum.go
@@ -11,15 +11,3 @@ func main() {
 func sum(a int, b int) int {
 	return a + b
 }
-
-func sub(a int, b int) int {
-	return a - b
-}
-
-func times(a int, b int) int {
-	return a * b
-}
-
-func sumX(a int, b int) int {
-	return a + b + a
-}
